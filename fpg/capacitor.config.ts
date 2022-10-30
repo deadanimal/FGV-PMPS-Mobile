c65d7c5ba6a86efdfeca27fd8464655146f94124
Amir <amirrasyid@outlook.com>
2022-10-30 22:23:10 +0800

    Initial Framework

---
+++ fpg/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fpg',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
