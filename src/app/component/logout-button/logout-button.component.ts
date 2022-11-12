1c1e494d82503e4672a4f48ce8bb575eafbd52ff
Amir <amirrasyid@outlook.com>
2022-11-13 03:00:20 +0800

    Logout Button design

--- src/app/component/logout-button/logout-button.component.ts
+++ src/app/component/logout-button/logout-button.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PopoverController } from '@ionic/angular';
 import { StorageService } from 'src/app/service/storage.service';
 
 @Component({
@@ -12,13 +13,16 @@ export class LogoutButtonComponent implements OnInit {
   constructor(
     private storageService:StorageService,
     private router:Router,
+    private popoverController:PopoverController,
   ) { }
 
   ngOnInit() {
   }
 
-  logout(){
+  async logout(){
     this.storageService.eraseAll();
+    let popoverCtrl = await this.popoverController.getTop();
+    popoverCtrl.dismiss();
     this.router.navigateByUrl('/login',{
       replaceUrl : true
     });
